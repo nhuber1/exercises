abb5d42bb073a5e9e27c185ab4f60767c13bd0d7
root <root@DESKTOP-RBR7681.localdomain>
2018-05-02 21:49:05 -0700

    finished exercise 3

--- ch9-data-frames/exercise-3/exercise.R
+++ ch9-data-frames/exercise-3/exercise.R
@@ -2,43 +2,48 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+expenditure <- data.frame(USPersonalExpenditure, stringsAsFactors = F)
 
 # What are the column names of your dataframe?
-
+colnames(expenditure)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(expenditure)
 
 # Add a column "category" to your data frame that contains the rownames
-
+expenditure$category <- rownames(expenditure)
 
 # How much money was spent on personal care in 1940?
-
-
+expenditure[expenditure$category == "Personal Care", "X1940"]
+#1.04 whats? million dollars?
 # How much money was spent on Food and Tobacco in 1960?
-
+expenditure[expenditure$category == "Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
-
+expenditure[expenditure$X1960 == max(expenditure$X1960), "category"]
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
-
+lowest_category <- function(year){
+  column <- paste0("X", year)
+  result <- expenditure[expenditure[, column] == min(expenditure[, column]), "category"]
+  result
+}
 
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
+sapply(seq(1940, 1960, 5), lowest_category)
 
